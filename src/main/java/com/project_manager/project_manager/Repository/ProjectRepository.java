05c1510f91aaac115b688b8eb5918e2dc93e3404
Armorplate <yarchuk-maks@mail.ru>
2024-09-28 02:32:50 +0300

    update project

--- src/main/java/com/project_manager/project_manager/Repository/ProjectRepository.java
+++ src/main/java/com/project_manager/project_manager/Repository/ProjectRepository.java
@@ -1,14 +1,12 @@
 package com.project_manager.project_manager.Repository;
 
-import com.project_manager.project_manager.entities.User;
+import com.project_manager.project_manager.entities.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findByEmail(String email);
+public interface ProjectRepository extends JpaRepository<Project, UUID> {
+
 }
